c52499ad6d923c8b26b1ef18c902ed6f609233db
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-01 14:57:20 -0500

    new coomit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Kal";
+            int age = 26;
+            double height = 5.51;
+            bool boring = false;
+            char initial = 'K';
+            decimal Mydecimal = 2.3m;
+
+
+
+            Console.WriteLine($"Hello, my name is {name}. I am {age} years old");
+            Console.WriteLine($"My height is {height}");
+            Console.WriteLine($"Is coding boring?:  {boring}");
+            Console.WriteLine($"my name's initial is {initial}");
+            Console.WriteLine($"a random decmial number: {Mydecimal}");
+
+
+
+
+
+
         }
     }
 }
